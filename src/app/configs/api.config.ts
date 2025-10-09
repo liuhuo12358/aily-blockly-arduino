0ac6d8c47935f22a6c5ce62eab770bd7ee9fe0f1
stao <werewolf_st@hotmail.com>
2025-10-09 17:54:21 +0800

    切换为开发服务器

--- src/app/configs/api.config.ts
+++ src/app/configs/api.config.ts
@@ -1,7 +1,7 @@
 const SERVER_URL: string = 'https://aily-chat.diandeng.tech';
-const CHAT_SERVER_URL: string = 'http://127.0.0.1:8000';
-const AUTH_SERVER_URL: string = 'http://127.0.0.1:8001';
-const WORKSPACE_SERVER_URL: string = 'http://127.0.0.1:8002';
+const CHAT_SERVER_URL: string = 'http://114.132.150.141:8000';
+const AUTH_SERVER_URL: string = 'http://114.132.150.141:8001';
+const WORKSPACE_SERVER_URL: string = 'http://114.132.150.141:8002';
 const REGISTRY_URL: string = 'https://registry.diandeng.tech';
 
 export const API = {
