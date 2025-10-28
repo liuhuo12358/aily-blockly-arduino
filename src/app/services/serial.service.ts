36fe5b0fbd5683706b60168fe01b01fc78ac65eb
aily <aily@example.com>
2025-10-28 16:53:32 +0800

    调整串口获取

--- src/app/services/serial.service.ts
+++ src/app/services/serial.service.ts
@@ -16,9 +16,16 @@ export class SerialService {
   // 此处还未考虑linux、macos适配
   async getSerialPorts(): Promise<PortItem[]> {
     if (this.electronService.isElectron) {
-      let serialList = (await window['SerialPort'].list()).map((item) => {
-        let friendlyName: string = item.friendlyName.replace(/ \(COM\d+\)$/, '');
-        let keywords = ["蓝牙", "ble", "bluetooth"];
+      const currentSerialPortList = await window['SerialPort'].list();
+      
+      console.log('currentSerialPortList', currentSerialPortList);
+
+      let serialList: PortItem[] = [];
+
+      if (window['platform'].isWindows) {
+        serialList = currentSerialPortList.map((item) => {
+          let friendlyName: string = item.friendlyName.replace(/ \(COM\d+\)$/, '');
+          let keywords = ["蓝牙", "ble", "bluetooth"];
         let icon: string = keywords.some(keyword => item.friendlyName.toLowerCase().includes(keyword.toLowerCase())) ? "fa-light fa-bluetooth" : 'fa-light fa-usb-drive';
         return {
           name: item.path,
@@ -27,6 +34,25 @@ export class SerialService {
           icon: icon,
         }
       });
+      } else if (window['platform'].isMacOS) {
+        // 只返回usb串口设备
+        serialList = currentSerialPortList.map((item) => {
+          let friendlyName: string = item.manufacturer? item.manufacturer : item.path.replace('/dev/tty.usbserial-', '').replace('/dev/cu.usbserial-', '');
+          let keywords = ["usb", "serial", "uart", "ftdi", "ch340", "cp210x"];
+          let icon: string = keywords.some(keyword => item.path.toLowerCase().includes(keyword.toLowerCase())) ? "fa-light fa-usb-drive" : 'fa-light fa-computer';
+          return {
+            name: item.path,
+            text: friendlyName,
+            type: 'serial',
+            icon: icon,
+          }
+        });
+      } else if (window['platform'].isLinux) {
+        //
+      }
+
+      console.log('serialList', serialList);
+      
       return serialList;
     } else {
       const port = await navigator['serial'].requestPort();
